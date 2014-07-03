cdaf081ba8d9586bd099928ba80b5687af01277a
Ace Subido and Karlo Nicholas Soriano <dev+ace+karlo@aelogica.com>
2014-07-03 08:26:38 +0800

    Add asset paths for precompilation in production

--- lib/theme_essay/engine.rb
+++ lib/theme_essay/engine.rb
@@ -1,5 +1,15 @@
 module ThemeEssay
   class Engine < ::Rails::Engine
     isolate_namespace ThemeEssay
+
+    initializer :assets do |config|
+      engine_assets_path = File.join(File.dirname(__FILE__), '..', '..', 'app', 'assets')
+      all_assets = Dir.glob File.join(engine_assets_path, 'stylesheets', '**', '*.css*')
+      all_assets += Dir.glob File.join(engine_assets_path, 'javascripts', '**', '*.js*')
+      all_assets.each {|path| path.gsub!("#{engine_assets_path}/stylesheets/", '')}
+      all_assets.each {|path| path.gsub!("#{engine_assets_path}/javascripts/", '')}
+      all_assets.each {|path| path.gsub!(/.(scss|coffee|sass)$/, '')}
+      Rails.application.config.assets.precompile += all_assets
+    end
   end
 end
